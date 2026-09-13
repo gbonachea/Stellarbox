55e87f1b9fff53e123f08891252bdd957139c240
Gus <gus.bonachea@aol.com>
2026-09-12 21:51:01 -0300

    Add files via upload

---
+++ stellarbox/obj-x86_64-linux-gnu/CMakeFiles/stellarbox.dir/openbox/actions/all.c.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/stellarbox.dir/openbox/actions/all.c.o: \
+ /home/gus/Downloads/Proyect/Openbox/openbox/actions/all.c \
+ /usr/include/stdc-predef.h \
+ /home/gus/Downloads/Proyect/Openbox/openbox/actions/all.h
