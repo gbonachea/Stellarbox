2a58f287002b54d5f9f00f38de5ad3ea79cc9a69
Gus <gus.bonachea@aol.com>
2026-09-12 21:45:41 -0300

    Add files via upload

---
+++ stellarbox/obj-x86_64-linux-gnu/CMakeFiles/stellarbox.dir/DependInfo.cmake
@@ -0,0 +1,98 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions.c" "CMakeFiles/stellarbox.dir/openbox/actions.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/addremovedesktop.c" "CMakeFiles/stellarbox.dir/openbox/actions/addremovedesktop.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/addremovedesktop.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/all.c" "CMakeFiles/stellarbox.dir/openbox/actions/all.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/all.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/breakchroot.c" "CMakeFiles/stellarbox.dir/openbox/actions/breakchroot.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/breakchroot.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/close.c" "CMakeFiles/stellarbox.dir/openbox/actions/close.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/close.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/cyclewindows.c" "CMakeFiles/stellarbox.dir/openbox/actions/cyclewindows.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/cyclewindows.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/debug.c" "CMakeFiles/stellarbox.dir/openbox/actions/debug.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/debug.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/decorations.c" "CMakeFiles/stellarbox.dir/openbox/actions/decorations.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/decorations.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/desktop.c" "CMakeFiles/stellarbox.dir/openbox/actions/desktop.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/desktop.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/directionalwindows.c" "CMakeFiles/stellarbox.dir/openbox/actions/directionalwindows.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/directionalwindows.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/dock.c" "CMakeFiles/stellarbox.dir/openbox/actions/dock.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/dock.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/dockautohide.c" "CMakeFiles/stellarbox.dir/openbox/actions/dockautohide.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/dockautohide.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/execute.c" "CMakeFiles/stellarbox.dir/openbox/actions/execute.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/execute.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/exit.c" "CMakeFiles/stellarbox.dir/openbox/actions/exit.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/exit.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/focus.c" "CMakeFiles/stellarbox.dir/openbox/actions/focus.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/focus.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/focusfallback.c" "CMakeFiles/stellarbox.dir/openbox/actions/focusfallback.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/focusfallback.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/focustobottom.c" "CMakeFiles/stellarbox.dir/openbox/actions/focustobottom.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/focustobottom.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/fullscreen.c" "CMakeFiles/stellarbox.dir/openbox/actions/fullscreen.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/fullscreen.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/growtoedge.c" "CMakeFiles/stellarbox.dir/openbox/actions/growtoedge.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/growtoedge.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/iconify.c" "CMakeFiles/stellarbox.dir/openbox/actions/iconify.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/iconify.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/if.c" "CMakeFiles/stellarbox.dir/openbox/actions/if.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/if.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/kill.c" "CMakeFiles/stellarbox.dir/openbox/actions/kill.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/kill.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/layer.c" "CMakeFiles/stellarbox.dir/openbox/actions/layer.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/layer.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/lower.c" "CMakeFiles/stellarbox.dir/openbox/actions/lower.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/lower.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/maximize.c" "CMakeFiles/stellarbox.dir/openbox/actions/maximize.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/maximize.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/move.c" "CMakeFiles/stellarbox.dir/openbox/actions/move.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/move.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/moverelative.c" "CMakeFiles/stellarbox.dir/openbox/actions/moverelative.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/moverelative.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/moveresizeto.c" "CMakeFiles/stellarbox.dir/openbox/actions/moveresizeto.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/moveresizeto.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/movetoedge.c" "CMakeFiles/stellarbox.dir/openbox/actions/movetoedge.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/movetoedge.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/omnipresent.c" "CMakeFiles/stellarbox.dir/openbox/actions/omnipresent.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/omnipresent.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/raise.c" "CMakeFiles/stellarbox.dir/openbox/actions/raise.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/raise.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/raiselower.c" "CMakeFiles/stellarbox.dir/openbox/actions/raiselower.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/raiselower.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/reconfigure.c" "CMakeFiles/stellarbox.dir/openbox/actions/reconfigure.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/reconfigure.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/resize.c" "CMakeFiles/stellarbox.dir/openbox/actions/resize.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/resize.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/resizerelative.c" "CMakeFiles/stellarbox.dir/openbox/actions/resizerelative.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/resizerelative.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/restart.c" "CMakeFiles/stellarbox.dir/openbox/actions/restart.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/restart.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/shade.c" "CMakeFiles/stellarbox.dir/openbox/actions/shade.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/shade.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/shadelowerraise.c" "CMakeFiles/stellarbox.dir/openbox/actions/shadelowerraise.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/shadelowerraise.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/showdesktop.c" "CMakeFiles/stellarbox.dir/openbox/actions/showdesktop.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/showdesktop.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/showmenu.c" "CMakeFiles/stellarbox.dir/openbox/actions/showmenu.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/showmenu.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/actions/unfocus.c" "CMakeFiles/stellarbox.dir/openbox/actions/unfocus.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/actions/unfocus.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/client.c" "CMakeFiles/stellarbox.dir/openbox/client.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/client.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/client_list_combined_menu.c" "CMakeFiles/stellarbox.dir/openbox/client_list_combined_menu.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/client_list_combined_menu.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/client_list_menu.c" "CMakeFiles/stellarbox.dir/openbox/client_list_menu.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/client_list_menu.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/client_menu.c" "CMakeFiles/stellarbox.dir/openbox/client_menu.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/client_menu.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/config.c" "CMakeFiles/stellarbox.dir/openbox/config.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/config.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/debug.c" "CMakeFiles/stellarbox.dir/openbox/debug.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/debug.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/dock.c" "CMakeFiles/stellarbox.dir/openbox/dock.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/dock.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/event.c" "CMakeFiles/stellarbox.dir/openbox/event.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/event.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/focus.c" "CMakeFiles/stellarbox.dir/openbox/focus.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/focus.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/focus_cycle.c" "CMakeFiles/stellarbox.dir/openbox/focus_cycle.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/focus_cycle.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/focus_cycle_indicator.c" "CMakeFiles/stellarbox.dir/openbox/focus_cycle_indicator.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/focus_cycle_indicator.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/focus_cycle_popup.c" "CMakeFiles/stellarbox.dir/openbox/focus_cycle_popup.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/focus_cycle_popup.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/frame.c" "CMakeFiles/stellarbox.dir/openbox/frame.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/frame.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/framerender.c" "CMakeFiles/stellarbox.dir/openbox/framerender.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/framerender.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/grab.c" "CMakeFiles/stellarbox.dir/openbox/grab.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/grab.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/group.c" "CMakeFiles/stellarbox.dir/openbox/group.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/group.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/keyboard.c" "CMakeFiles/stellarbox.dir/openbox/keyboard.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/keyboard.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/keytree.c" "CMakeFiles/stellarbox.dir/openbox/keytree.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/keytree.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/menu.c" "CMakeFiles/stellarbox.dir/openbox/menu.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/menu.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/menuframe.c" "CMakeFiles/stellarbox.dir/openbox/menuframe.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/menuframe.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/mouse.c" "CMakeFiles/stellarbox.dir/openbox/mouse.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/mouse.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/moveresize.c" "CMakeFiles/stellarbox.dir/openbox/moveresize.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/moveresize.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/openbox.c" "CMakeFiles/stellarbox.dir/openbox/openbox.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/openbox.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/ping.c" "CMakeFiles/stellarbox.dir/openbox/ping.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/ping.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/place.c" "CMakeFiles/stellarbox.dir/openbox/place.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/place.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/place_overlap.c" "CMakeFiles/stellarbox.dir/openbox/place_overlap.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/place_overlap.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/popup.c" "CMakeFiles/stellarbox.dir/openbox/popup.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/popup.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/prompt.c" "CMakeFiles/stellarbox.dir/openbox/prompt.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/prompt.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/resist.c" "CMakeFiles/stellarbox.dir/openbox/resist.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/resist.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/screen.c" "CMakeFiles/stellarbox.dir/openbox/screen.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/screen.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/session.c" "CMakeFiles/stellarbox.dir/openbox/session.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/session.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/stacking.c" "CMakeFiles/stellarbox.dir/openbox/stacking.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/stacking.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/startupnotify.c" "CMakeFiles/stellarbox.dir/openbox/startupnotify.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/startupnotify.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/translate.c" "CMakeFiles/stellarbox.dir/openbox/translate.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/translate.c.o.d"
+  "/home/gus/Downloads/Proyect/Openbox/openbox/window.c" "CMakeFiles/stellarbox.dir/openbox/window.c.o" "gcc" "CMakeFiles/stellarbox.dir/openbox/window.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ stellarbox/obj-x86_64-linux-gnu/CMakeFiles/stellarbox.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for stellarbox.
+# This may be replaced when dependencies are built.
